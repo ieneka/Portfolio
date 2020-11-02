9c46c9a037b0130ceb875cbb77459c96db57d1d2
ieneka <60981008+ieneka@users.noreply.github.com>
2020-11-02 12:58:18 +0100

    Portfolio 2.0

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule} from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
